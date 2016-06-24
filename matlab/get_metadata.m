039746b322549a2053f217282bb6b4c3fd411845
Alexander Navarro <anavarro@IMAC-ATI0062.local>
2016-06-24 15:49:19 +0100

    minor debugging

--- matlab/get_metadata.m
+++ matlab/get_metadata.m
@@ -32,8 +32,8 @@ for name_entry = 1:size(names, 1)
     
     % Search for a description match
     desc_entry = 1;
-    while ~strcmp(description{desc_entry, 2}, names{name_entry}) &&
-        desc_entry <= n_desc
+    while ~strcmp(description{desc_entry, 2}, names{name_entry}) && ...
+            desc_entry <= n_desc
         desc_entry = desc_entry + 1;
     end
     
