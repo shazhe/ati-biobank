22d5ea543e4b2820c00715ec7001dab261f54f13
Alexander Navarro <anavarro@IMAC-ATI0062.local>
2016-06-27 15:29:24 +0100

    minor changes

--- matlab/get_var.m
+++ matlab/get_var.m
@@ -1,10 +1,12 @@
-function col = get_var(mat, names, varname)
+function var = get_var(mat, names, varname)
 % GET_VAR Gets the variable corresponding to a column of matrix of
 % data
     for col = 1:length(names)
         code = strsplit(names{col}, '-');
-        if strcmp(code{1}, 'varname')
+        if strcmp(code{1}, varname)
             break
         end
     end
+    
+    var = mat(:, col);
 end
