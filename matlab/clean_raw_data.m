66235def33bef8c6e7bf59f68fcc60b54ad95d29
Alexander Navarro <anavarro@IMAC-ATI0062.local>
2016-06-27 10:27:17 +0100

    Minor changes

---
+++ matlab/clean_raw_data.m
@@ -0,0 +1,42 @@
+%% CLEAN_RAW_DATA.m
+% Load and clean up the raw, not deconfounded data, then save it
+%     --- still within the server --- 
+% to unify the set we will be working on.
+
+addpaths;   % Adds the relevant paths
+loady;      % Loads the raw data file
+
+% Create some aliases for interpretability
+names = varsVARS;
+keep = varskeep;
+raw = varsdraw;
+desc = varsHTML;
+
+fprintf('Merging visits... ');
+[merged, u_names] = merge_visits(raw, keep, names);
+fprintf('OK!\n');
+
+fprintf('Fixing missing data... ');
+data = fix_missing(merged);
+fprintf('OK!\n');
+
+fprintf('Getting metadata... ');
+[meta, no_desc] = get_metadata(u_names, desc);
+fprintf('OK!\n');
+
+fprintf('Saving... ');
+
+%path = cd('/vols/Data/HCP/BBUK/');
+
+save('cleanedRawDataset.mat', ...
+     'data', 'merged', 'raw', 'keep', 'names', 'u_names', 'meta', ...
+     'no_desc');
+
+
+%cd(path);
+
+fprintf('All done! :D \n')
+
+clear; % Remove all non-important data
+
+load_clean;
