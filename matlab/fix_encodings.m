6883e6e945018d340396867e8c17279939b1e088
Alex Navarro <anavarro@turing.ac.uk>
2016-07-04 13:12:54 +0100

    Minor changes

--- matlab/fix_encodings.m
+++ matlab/fix_encodings.m
@@ -10,31 +10,61 @@ function data = fix_encodings(data, rule)
 %     rule: is a 1 x M vector with the encoded treatment following
 %
 % See also: fill_nested, prep_missing
-    switch rule
-      case 1 % Negative values are missing
-        data(data < 0) = NaN;
-      case 2 % No missing values encoded
-        data(data < 0) = NaN;
-      case 3 % Not applicable: -2; Missing: -1 and -3
-        data(data == -2) = Inf;
-        data(data == -1) = NaN;
-        data(data == -3) = NaN;
-      case 4 % Missing: -1 and -3; Less than one: -10
-        data(data == -1) = NaN;
-        data(data == -3) = NaN;
-        data(data == -10) = 0;
-      case 5 % Missing: 222 and 313
-        data(data == 222) = NaN;
-        data(data == 313) = NaN;
-      case 6 % Missing: 6 and negative values
-        data(data == 6) = NaN;
-        data(data <  0) = NaN;
-      otherwise % Unkown rule case
-        Error = MException('fix_encodings:InvalidAction',...
-                           'Unkown rule value %d.', ...
-                           rule(var));
+    
+    n_data = size(data, 1);
+    
+    for entry = 1:n_x
         
-        throw(Error);
+        if rule(entry) == 1 % 'Missing: -1 and -3'
+            data(data == -1) = NaN;
+            data(data == -3) = NaN;
+            
+        elseif rule(entry) == 2 % 'Missing: -1 and -3/ Less than one: -10'
+            data(data == -1) = NaN;
+            data(data == -3) = NaN;
+            data(data == -10) = 0;
+            
+        elseif rule(entry) == 3 % 'Missing: -1 and -3/ Not applicable: -2'
+            data(data == -2) = Inf;
+            data(data == -1) = NaN;
+            data(data == -3) = NaN;
+            
+        elseif rule(entry) == 4 % 'Missing: -1 and -3/ Not applicable: 99'
+            data(data == 99) = Inf;
+            data(data == -1) = NaN;
+            data(data == -3) = NaN;
+            
+        elseif rule(entry) == 5 % 'Missing: 222 and 313'
+            data(data == 222) = NaN;
+            data(data == 313) = NaN;
+            
+        elseif rule(entry) == 6 %  'Missing: 352'
+            data(data == 352) = NaN;
+            
+        elseif rule(entry) == 7 % 'Missing: 5 and negative values'
+            data(data == 5) = NaN;
+            data(data <  0) = NaN;
+            
+        elseif rule(entry) == 8 % 'Missing: 6 and negative values'
+            data(data == 6) = NaN;
+            data(data <  0) = NaN;
+            
+        elseif rule(entry) == 9 % 'Missing: 9'
+            data(data == 9) = NaN;
+            
+        elseif rule(entry) == 10 % 'Negative values are missing'
+            data(data < 0) = NaN;
+            
+        elseif rule(entry) == 11 % 'No missing values encoded'
+            data(data < 0) = NaN;
+            
+        else
+            Error = MException('fill_nested:InvalidAction',...
+                               'Unkown rule value %d.', ...
+                               rule(var));
+            throw(Error);
+        end
+        
     end
 
 end
