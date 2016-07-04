b0724d7ac691d50e8515f4f9af8a37617c944fca
Alex Navarro <anavarro@turing.ac.uk>
2016-07-04 16:16:26 +0100

    Minor changes

--- matlab/merge_visits.m
+++ matlab/merge_visits.m
@@ -19,63 +19,71 @@ function [merged, u_names]= merge_visits(data, keep, bb_names, ...
         keep = unique(bb_names, 'stable');
     end
 
-
     if nargin < 5
         displayNaN = true;              % display output.
     end
 
     % get unique items
-    bb_names_keep = bb_names(keep);
-    u_names = unique(bb_names(keep), 'stable');
+    names_keep = bb_names(keep);
+    u_names = unique(names_keep, 'stable');
     n_names = length(u_names);
-
     
+    num_nans = 0;
     n_subjs = size(data,1);           % number of subjects in the data
     merged = zeros(n_subjs, n_names); % preallocate merged matrix
-    num_nans = 0;                     % get the number of nans
 
     % Loop through all available names
     for name_entry = 1:n_names
         % get the variable name irrespective of the visits
         var_name = u_names(name_entry);
-        var_idx = bb_names_keep == var_name;
+        var_idx = names == var_name;
         
         % Loop through subjects
         for subject = 1:n_subjs
-            % get all raw entries.
-            raw_entries = data(subject, var_idx);
             
-            % Get the last visit without removing nans
-            if strcmp(method, 'visit')
+            raw_entries = data(subject, var_idx); % all visits for
+                                                  % a subject
+            
+            if strcmp(method, 'visit') % last visit w/ nans
                 merged(subject, name_entry) = raw_entries(end);
+                
             else
-                % remove NaNs from the raw entries
+                % Methods that remove nans
                 raw_entries = raw_entries(~isnan(raw_entries));
                 
-                % Apply the merging function
                 if ~isempty(raw_entries)
-                    % There are valid entries to consolidate
                     if strcmp(method, 'mean')           
-                        merged(subject, name_entry) = mean(raw_entries);
+                        merged(subject, name_entry) = ...
+                            mean(raw_entries);
+                        
                     elseif strcmp(method, 'last')
-                        merged(subject, name_entry) = raw_entries(end);
+                        merged(subject, name_entry) = ...
+                            raw_entries(end);
+                        
                     elseif strcmp(method, 'first')
-                        merged(subject, name_entry) = raw_entries(1);
+                        merged(subject, name_entry) = ...
+                            raw_entries(1);
+                        
                     else
-                        fprint(' !!! ERROR !!! Method not implemented!\n');
+                        fprint([' !!! ERROR !!! Method not ' ...
+                                'implemented!\n']);
                     end
                 else
                     % No valid entry, thus keep it at NaN
                     merged(subject, name_entry) = NaN;
-                    num_nans = num_nans + 1;
                 end
-                
             end
+            
+            if isnan(merged(subject, name_entry))
+                num_nans = num_nans + 1;
+            end
+            
         end
     end
 
     if num_nans > 0 && displayNaN
-        fprintf(['Total number of NaN entries:', num2str(num_nans),'\n']);
+        fprintf(['Total number of NaN entries: ', num2str(num_nans), ...
+                 ' (= ', num2str(num_nans / numel(merged(:)) * 100), '\n']);
     end
 
 end
