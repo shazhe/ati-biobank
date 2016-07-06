1de2022c9ad0c2dbf75e6c3d18f59988b81481e9
Alex Navarro <Alex Navarro>
2016-07-06 13:42:00 +0100

    Corrected process_visits.m

--- matlab/util/process_visits.m
+++ matlab/util/process_visits.m
@@ -1,8 +1,7 @@
-function [cube, u_names] = process_visits(data, keep, bb_names,)
-% MERGE_VISITS Merges the different visits into a single index.
+function [cube, u_names] = process_visits(data, keep, bb_names)
+% PROCESS_VISITS Merges the different visits into a single index.
 %
-%   USAGE: merged = process_visits(data, keep, bb_names, ...
-%				     method, displayNaN)
+%   USAGE: merged = process_visits(data, keep, bb_names)
 %
 %   where:
 %       data is the dataset with all visits
