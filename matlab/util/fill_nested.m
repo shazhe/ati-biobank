33b460f91b52d6ebffbeea94e6455564cd710967
Zhe Sha <zhesha1006@gmail.com>
2016-07-07 15:13:40 +0100

    modified fill_nested to get names after removing the variables

--- matlab/util/fill_nested.m
+++ matlab/util/fill_nested.m
@@ -1,12 +1,13 @@
-function [data, names] = fill_nested(varargin)
+function [data, ru_names] = fill_nested(varargin)
 % FILL_NESTED Fills in the missing data due to nested queries in
 % the biobank (that is, missing data in variables that only occur
 % when you supply a specific answer in a parent question).
 %
 % USAGE: data = fill_nested(data, parents, rule, params)
-% 
+%
 % where:
 %     data: is a N subjects x  M variables data matrix.
+%     ru_names: all the variable names after removing the variables.
 %     parent: is a 1 x M vector indicating the parents of a given
 %              variable (0 if root parent).
 %     rule: is a 1 x M vector with the encoded treatment following
@@ -25,37 +26,38 @@ function [data, names] = fill_nested(varargin)
     bbuk_levels = varargin{7};
     new_levels = varargin{8};
     processing = varargin{9};
-    
+
     [n_subs, n_vars, n_visits] = size(data);
-    
+
     var = 1;
+    ru_names = u_names;
     while var <= n_vars
-        
+
         if processing(var) == 1 % Only gaussianise
-            
+
             % Get parent information
             if parent1(var) ~= 0 && parent2(var) ~= 0
                 data = inherit(data, var, u_names, ...
                                parent1(var), parent2(var), ...
                                parval1{var}, parval2{var});
             end
-            
+
             % Change levels if needed
             data(:, var, :) = change_encoding(data(:, var, :), ...
                                               bbuk_levels{var},...
                                               new_levels{var});
-            
+
         elseif processing(var) == 2 % Remove
-            
+
             % Remove variable
             idx = 1:n_vars ~= var;
             data = data(:,idx, :);
-            u_names = u_names(idx);
-            
+            ru_names = ru_names(idx);
+
             n_vars = n_vars - 1;
-            
+
         elseif processing(var) == 3 % Set Missing to 0 and gaussianise
-            
+
             % Get parent information
             if parent1(var) ~= 0 && parent2(var) ~= 0
                 data = inherit(data, var, u_names, ...
@@ -76,7 +78,7 @@ function [data, names] = fill_nested(varargin)
                                processing(var));
             throw(Error);
         end
-        
+
         var = var + 1;
     end
 end
