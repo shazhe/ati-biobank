cb8b7f40f3b5131b61f623cb6fa9324c1ba9364e
Zhe Sha <zhesha1006@gmail.com>
2016-07-12 10:11:55 +0100

    update scripts

--- matlab/src/selectVars.m
+++ matlab/src/selectVars.m
@@ -23,7 +23,7 @@ fprintf('OK!\n');
 %  3 Confound/factors for interpretaion
 %  4 misc
 
-V_in{1}  =  [1329 2003 6155 1239 1249 1558 864 884 904 1170 1160 2080 2050 21001 ...
+V_in{1}  =  [1329 20003 6155 1239 1249 1558 864 884 904 1170 1160 2080 2050 21001 ...
           20002 4079 4080 2443 1031 6160 2247 2257 ];
 
 V_in{2} = [20016 20023 398 20018];
@@ -34,5 +34,22 @@ V_in{4} = [6152 6153];
 
 Vin = horzcat(V_in{1}, V_in{2}, V_in{3}, V_in{4}); % 34 in total
 
-[~, ~, idxin] = intersect(Vin, allnames); % This gives me only 24 variables, why?
+idxin = [];
+namesin = [];
+namesout = [];
+for i  = 1: length(Vin)
+    idxtemp =  find(allnames == Vin(i));
+    idxin = [idxin idxtemp];
+    if length(idxtemp) > 0
+        namesin = [namesin repmat(Vin(i), 1, length(idxtemp))];
+    else
+        namesout = [namesout Vin(i)];
+    end
+end
+
+namesout % there are 10 variables not in the original 7404 variables.
 Vkeep = dirty(:, idxin);
+Vkeep = [namesin; Vkeep];
+
+% Save this matrix into a csv file which will be used in other software
+csvwrite('selectVars.csv', Vkeep);
